76ea41e19c1e6badd1f903adf3c59b041055d074
FantRS <rostykkashper2@gmail.com>
2025-10-26 01:26:07 +0300

    AppData update

--- server/src/core/app_data.rs
+++ server/src/core/app_data.rs
@@ -1,12 +1,45 @@
 use sqlx::PgPool;
 
+use crate::{
+    app::models::ws_rooms::Rooms,
+    core::app_error::{AppError, AppResult},
+};
+
 #[derive(Clone)]
 pub struct AppData {
     pub pool: PgPool,
+    pub rooms: Rooms,
 }
 
 impl AppData {
-    pub fn new(pool: PgPool) -> Self {
-        Self { pool }
+    pub fn builder() -> AppDataBuilder {
+        AppDataBuilder::default()
+    }
+}
+
+#[derive(Default)]
+pub struct AppDataBuilder {
+    pub pool: Option<PgPool>,
+    pub rooms: Option<Rooms>,
+}
+
+impl AppDataBuilder {
+    pub fn build(self) -> AppResult<AppData> {
+        let data = AppData {
+            pool: self.pool.ok_or(AppError::InternalServer("PgPool not set".into()))?,
+            rooms: self.rooms.ok_or(AppError::InternalServer("Rooms not set".into()))?,
+        };
+
+        Ok(data)
+    }
+
+    pub fn with_pool(mut self, pool: PgPool) -> Self {
+        self.pool = Some(pool);
+        self
+    }
+
+    pub fn with_rooms(mut self, rooms: Rooms) -> Self {
+        self.rooms = Some(rooms);
+        self
     }
 }
